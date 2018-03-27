a48523cf8a0189dece05a5455ebbbee4a1f450d4
RonSala <ron.sala@gmail.com>
2018-03-27 05:28:38 -0400

    Update question.rb

--- lib/question.rb
+++ lib/question.rb
@@ -4,7 +4,7 @@ require 'nokogiri'
 
 class Question
 
-  attr_accessor :word, :a, :b, :c, :d
+  attr_accessor :word, :a, :b, :c, :d, :lang
 
   def initialize(word, a, b, c, d)
     @word = word
@@ -12,51 +12,24 @@ class Question
     @b = b
     @c = c
     @d = d
+    puts "#{@current_player}, what is #{@word}?"
   end
 
-  def random_lang
-    random_page = Nokogiri::HTML(open("https://en.wiktionary.org/wiki/Special:Random"))
-    lang = random_page.css("h2 .mw-headline").text
+  def self.random_entry
+    random_entry = Nokogiri::HTML(open("https://en.wiktionary.org/wiki/Special:Random"))
+    @lang = random_entry.css("h2 .mw-headline").text
+  end
+
+  def self.english
     binding.pry
+    if @lang == "English" 
+      @word = @lang
+    else
+      self.random_entry
+    end
   end
 
-  def english?
-    random_lang
-    if lang == "English" word 
-  end
+  self.english
 
-  rand_english
 
-#   def self.scrape_index_page(index_url)
-#     scraped_students = []
-#     html = Nokogiri::HTML(open(index_url))
-#     html.css("div.student-card").each do |card|
-#       student = {
-#       :name => card.css(".student-name").text,
-#       :location => card.css(".student-location").text,
-#       :profile_url => card.css("a").attribute("href").value
-#     }
-#     scraped_students << student
-#     end
-#     scraped_students
-#   end
-
-#   def self.scrape_profile_page(profile_url)
-#     scraped_student = {}
-#     html = Nokogiri::HTML(open(profile_url))
-#     html.css(".vitals-container a").each do |a|
-#       if a.attribute("href").value =~ (/twitter/)
-#         scraped_student[:twitter] = a.attribute("href").value
-#       elsif a.attribute("href").value =~ (/linkedin/)
-#         scraped_student[:linkedin] = a.attribute("href").value
-#       elsif a.attribute("href").value =~ (/github/)
-#         scraped_student[:github] = a.attribute("href").value
-#       else
-#         scraped_student[:blog] = a.attribute("href").value
-#       end
-#     end
-#     scraped_student[:profile_quote] = html.css(".profile-quote").text
-#     scraped_student[:bio] = html.css(".description-holder p").text.split.join(" ")
-#     scraped_student
-#   end
 end
