538631cb4910f501a0b53784da04a9cc280f72c8
RonSala <ron.sala@gmail.com>
2018-03-28 02:21:21 -0400

    -mAdd PRNG

--- lib/question.rb
+++ lib/question.rb
@@ -8,23 +8,40 @@ class Question
 
   attr_accessor :word, :a, :b, :c, :d, :definition
 
-  # def initialize(word, a, b, c, d)
-  #   @word = word
-  #   @a = a
-  #   @b = b
-  #   @c = c
-  #   @d = d
-  #   puts "#{@current_player}, what is #{@word}?"
-  # end
+  def initialize
+    Question.random_entry
+    true_def = rand(1..4)
+    case true_def
+      when 1 
+        @a = @definition
+      when 2 
+        @b = @definition
+      when 3
+        @c = @definition
+      when 4 
+        @d = @definition
+    end
+    @word = word
+    # @a = a
+    # @b = b
+    # @c = c
+    # @d = d
+    # puts "#{@current_player}, what is #{@word}?"
+    puts @word
+    puts @a
+    puts @b
+    puts @c
+    puts @d
+  end
 
   def self.random_entry
     random_entry = Nokogiri::HTML(open("https://en.wiktionary.org/wiki/Special:RandomInCategory/English_lemmas#English"))
     @word = random_entry.css('#firstHeading').text
     @definition = random_entry.css('#mw-content-text div ol li').text.split("\n")[0]
-    binding.pry
   end
 
-  self.random_entry
-
+  # self.random_entry
 
 end
+
+test_q = Question.new
