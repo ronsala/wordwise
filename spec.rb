efaa19c11691dc251d00fdc10e6a63a71800884b
RonSala <ron.sala@gmail.com>
2018-03-29 02:15:31 -0400

    -mUpdate spec.rb

--- spec.rb
+++ spec.rb
@@ -2,5 +2,5 @@
 
 Specs:
 - [x] Have a CLI for interfacing with the application
-- [ ] Pull data from an external source
+- [X] Pull data from an external source
 - [ ] Implement both list and detail views
