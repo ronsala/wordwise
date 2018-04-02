04e293e7d70101622e906a258219bc037f1b91d3
RonSala <ron.sala@gmail.com>
2018-04-02 00:10:24 -0400

    Add scrapers for other word pages in Question.initialize

--- lib/question.rb
+++ lib/question.rb
@@ -11,13 +11,35 @@ class Question
     words = doc.css('td a')
     length = words.length
 
-    rand_word_no_1 = rand(0..length)
+    rand_word_no_1 = rand(0..length - 1)
     entry_url_1 = doc.css('td a')[rand_word_no_1].attribute('href').value.split(':').to_a.insert(1, 's:').join
     doc_1 = Nokogiri::HTML(open(entry_url_1))
     word_1 = doc_1.css('.hw').text
     definition_1 = doc_1.css('.ind').first.text
     origin_1 = doc_1.css('.senseInnerWrapper p').text
 
+    rand_word_no_2 = rand(0..length - 1)
+    entry_url_2 = doc.css('td a')[rand_word_no_2].attribute('href').value.split(':').to_a.insert(1, 's:').join
+    doc_2 = Nokogiri::HTML(open(entry_url_2))
+    word_2 = doc_2.css('.hw').text
+    definition_2 = doc_2.css('.ind').first.text
+    origin_2 = doc_2.css('.senseInnerWrapper p').text
+
+    rand_word_no_3 = rand(0..length - 1)
+    entry_url_3 = doc.css('td a')[rand_word_no_3].attribute('href').value.split(':').to_a.insert(1, 's:').join
+    doc_3 = Nokogiri::HTML(open(entry_url_3))
+    word_3 = doc_3.css('.hw').text
+    definition_3 = doc_3.css('.ind').first.text
+    origin_3 = doc_3.css('.senseInnerWrapper p').text
+
+    rand_word_no_4 = rand(0..length - 1)
+    entry_url_4 = doc.css('td a')[rand_word_no_4].attribute('href').value.split(':').to_a.insert(1, 's:').join
+    doc_4 = Nokogiri::HTML(open(entry_url_4))
+    word_4 = doc_4.css('.hw').text
+    definition_4 = doc_4.css('.ind').first.text
+    origin_4 = doc_4.css('.senseInnerWrapper p').text
+
+    definitions = [definition_1, definition_2, ]
 
 
     true_def = rand(1..4)
@@ -42,6 +64,15 @@ class Question
     puts word_1
     puts definition_1
     puts origin_1
+    puts word_2
+    puts definition_2
+    puts origin_2
+    puts word_3
+    puts definition_3
+    puts origin_3
+    puts word_4
+    puts definition_4
+    puts origin_4
     # puts a
     # puts b
     # puts c
