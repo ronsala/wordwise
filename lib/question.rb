09a8efc37bb1d4e51e5bcd69462b952fdbc0d9ca
RonSala <ron.sala@gmail.com>
2018-04-01 23:07:52 -0400

    Add definition page scraping

--- lib/question.rb
+++ lib/question.rb
@@ -3,39 +3,22 @@
 require 'open-uri'
 require 'pry'
 require 'nokogiri'
+require 'open_uri_redirections'
 
 class Question
 
-  # attr_accessor :word, :a, :b, :c, :d, :defin
-
   def initialize
-    # random_entry_1 = Nokogiri::HTML(open("https://en.wiktionary.org/wiki/Special:RandomInCategory/English_lemmas#English"))
-    # word = random_entry_1.css('#firstHeading').text
-    # defin_1 = random_entry_1.css('#mw-content-text div ol li').text.split("\n")[0]
-
-    # random_entry_2 = Nokogiri::HTML(open("https://en.wiktionary.org/wiki/Special:RandomInCategory/English_lemmas#English"))
-    # defin_2 = random_entry_2.css('#mw-content-text div ol li').text.split("\n")[0]
-
-    # random_entry_3 = Nokogiri::HTML(open("https://en.wiktionary.org/wiki/Special:RandomInCategory/English_lemmas#English"))
-    # defin_3 = random_entry_3.css('#mw-content-text div ol li').text.split("\n")[0]
-
-    # random_entry_4 = Nokogiri::HTML(open("https://en.wiktionary.org/wiki/Special:RandomInCategory/English_lemmas#English"))
-    # defin_4 = random_entry_4.css('#mw-content-text div ol li').text.split("\n")[0]
-
-    # random_entry_5 = Nokogiri::HTML(open("https://en.wiktionary.org/wiki/Special:RandomInCategory/English_lemmas#English"))
-    # defin_5 = random_entry_5.css('#mw-content-text div ol li').text.split("\n")[0]
-
     doc = Nokogiri::HTML(open("https://en.oxforddictionaries.com/explore/weird-and-wonderful-words"))
     words = doc.css('td a').to_a
     length = words.length
     rand_word_no = rand(0..length)
-    entry = doc.css('td a').to_a[rand_word_no]
-    entry_url = entry(\".+\")
-    word = entry.text
-    # defin_1 = random_entry_1.css('#mw-content-text div ol li').text.split("\n")[0]
-    # doc_2 = Nokogiri::HTML(open("entry"))
-    # defin_1 = doc_2.css('.ind').text
-
+    entry_url = doc.css('td a').to_a[rand_word_no].attribute('href').value
+    entry_url_split = entry_url.split(':').to_a
+    entry_url_s = entry_url_split.insert(1, 's:').join
+    doc_2 = Nokogiri::HTML(open(entry_url_s))
+    word = doc_2.css('.hw').text
+    definition = doc_2.css('.ind').first.text
+    origin = doc_2.css('.senseInnerWrapper p').text
 
     true_def = rand(1..4)
 
@@ -56,20 +39,14 @@ class Question
     # if c.nil? then c = defin_4 end
     # if d.nil? then d = defin_5 end
 
-    puts entry
-    puts entry_url
     puts word
-    # puts defin_1
+    puts definition
     # puts a
     # puts b
     # puts c
     # puts d
 
   end
-
 end
 
 test_q = Question.new
-
-# puts test_q.instance_variables
-# test_q.random_entry
