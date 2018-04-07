8f04386e9733e6824139c95d46e889080602a1b7
RonSala <ron.sala@gmail.com>
2018-04-06 23:58:51 -0400

    Add comments to Question

--- lib/question.rb
+++ lib/question.rb
@@ -9,18 +9,17 @@ class Question
   attr_accessor :word_1, :definition_1, :definition_2, :definition_3, :definition_4, :definitions, :definitions_s, :length, :origin_1, :rand_word_no_2, :word_nos
 
   def initialize
-    # @@word_nos.clear
     @word_nos = []
     doc = Nokogiri::HTML(open("https://en.oxforddictionaries.com/explore/weird-and-wonderful-words"))
     words = doc.css('td a')
     @length = words.length
     randomize
 
-     # Parses the individual words' web pages:
+     #Parses the individual words' web pages:
 
     entry_url_1 = doc.css('td a')[@word_nos[0]].attribute('href').value.split(':').to_a.insert(1, 's:').join
     # doc_1 = Nokogiri::HTML(open(entry_url_1))
-    doc_1 = Nokogiri::HTML(open("https://en.oxforddictionaries.com/definition/suedehead"))
+    doc_1 = Nokogiri::HTML(open("https://en.oxforddictionaries.com/definition/suedehead")) #Temporary: Test for absent word origin.
     @word_1 = doc_1.css('.hw').text.match(/^[a-zA-Z]+/)
     @definition_1 = doc_1.css('.ind').first.text
     @origin_1 = doc_1.css('.senseInnerWrapper p').text
@@ -43,14 +42,18 @@ class Question
     @definition_4 = doc_4.css('.ind').first.text
     origin_4 = doc_4.css('.senseInnerWrapper p').text
 
-    # Attempting conditional to check for empty strings.
+    #Attempting conditional to check for empty strings.
 
     if [@word_1, @word_2, @word_3, @word_4].any {|word| word.nil? then initialize} || @definitions.any {|definition| definition.nil? then initialize}
+
+    #Set instance variables for questions.
 
     @definition = @definition_1
     @definitions = [@definition_1, @definition_2, @definition_3, @definition_4]
     @definitions_s = @definitions.shuffle 
   end
+
+  #Return array of unique, pseudorandom index numbers to chose words from list on webpage.
 
   def randomize
     until @word_nos.length == 4
