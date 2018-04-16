9355af42470c73c050e77c0e6fa6752cc65af904
RonSala <ron.sala@gmail.com>
2018-04-15 21:09:54 -0400

    Refactor Question and Scraper

--- lib/question.rb
+++ lib/question.rb
@@ -1,62 +1,12 @@
-# 
-
+# Assemble a question consisting of a word and its definition shuffled with 3
+# other definitions.
 class Question
+  attr_accessor :word_1, :definition_1, :definition_2, :definition_3
+  attr_accessor :definition_4, :definitions, :length, :origin_1, :word_nos
 
-  attr_accessor :word_1, :definition_1, :definition_2, :definition_3, :definition_4, :definitions, :definitions_s, :length, :origin_1, :word_nos
-
+  # Set instance variables for Question objects.
   def initialize
-      
-        @word_nos = []
-        doc = Nokogiri::HTML(open("https://en.oxforddictionaries.com/explore/weird-and-wonderful-words"))
-        words = doc.css('td a')
-        @length = words.length
-        randomize
-
-        # Parses the individual words' web pages. The first will be the word in 
-        # the question. The rest are used to generate false definitions.:
-
-        entry_url_1 = doc.css('td a')[@word_nos[0]].attribute('href').value.split(':').to_a.insert(1, 's:').join
-        doc_1 = Nokogiri::HTML(open(entry_url_1))
-        @word_1 = doc_1.css('.hw').text.match(/^[a-zA-Z]+/)
-        @definition_1 = doc_1.css('.ind').first.text
-        @origin_1 = doc_1.css('.senseInnerWrapper p').text
-
-        entry_url_2 = doc.css('td a')[@word_nos[1]].attribute('href').value.split(':').to_a.insert(1, 's:').join
-        doc_2 = Nokogiri::HTML(open(entry_url_2))
-        word_2 = doc_2.css('.hw').text.match(/^[a-zA-Z]+/)
-        @definition_2 = doc_2.css('.ind').first.text
-
-        entry_url_3 = doc.css('td a')[@word_nos[2]].attribute('href').value.split(':').to_a.insert(1, 's:').join
-        doc_3 = Nokogiri::HTML(open(entry_url_3))
-        word_3 = doc_3.css('.hw').text.match(/^[a-zA-Z]+/)
-        @definition_3 = doc_3.css('.ind').first.text
-
-        entry_url_4 = doc.css('td a')[@word_nos[3]].attribute('href').value.split(':').to_a.insert(1, 's:').join
-        doc_4 = Nokogiri::HTML(open(entry_url_4))
-        word_4 = doc_4.css('.hw').text.match(/^[a-zA-Z]+/)
-        @definition_4 = doc_4.css('.ind').first.text
-
-    # Conditional to check for empty strings.
-
-    [@word_1, @word_2, @word_3, @word_4, @definition_1, @definition_2, @definition_3, @definition_4, @origin_1].each do |word| 
-      if word == "" 
-        initialize
-      end
-    end
-
-    # Set instance variables for Question objects.
-
     @definition = @definition_1
-    @definitions = [@definition_1, @definition_2, @definition_3, @definition_4]
-    @definitions_s = @definitions.shuffle 
-  end
-  # Return array of unique, pseudorandom index numbers to chose words from list 
-  # on webpage.
-
-  def randomize
-    until @word_nos.length == 4
-      rand_no = rand(0..@length - 1)
-      @word_nos.include?(rand_no) ? randomize : @word_nos << rand_no
-    end
+    @defs = [@def_1, @def_2, @def_3, @def_4].shuffle
   end
 end
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,52 +3,57 @@
 class Scraper
   attr_accessor :word_1, :definition_1, :definition_2, :definition_3
   attr_accessor :definition_4, :definitions, :definitions_s, :length, :origin_1
-  attr_accessor :word_nos
-  
+  attr_accessor :word_nos, :doc
+
   # Scrape word list page.
   def initialize
     @word_nos = []
-    doc = Nokogiri::HTML(open("https://en.oxforddictionaries.com/explore/weird-and-wonderful-words"))
+    @doc = Nokogiri::HTML(open("https://en.oxforddictionaries.com/explore/weird-and-wonderful-words"))
     words = doc.css('td a')
     @length = words.length
   end
 
-    # Parses the individual words' web pages. The first will be the word in 
-    # the question. The rest are used to generate false definitions.:
+  # Parses the individual words' web pages. The first will be the word in
+  # the question. The rest are used to generate false definitions.
+  def scrape_definitions
+    # entry_url_1 = doc.css('td a')[@word_nos[0]].attribute('href').value.split(':').to_a.insert(1, 's:').join
+    # doc_1 = Nokogiri::HTML(open(entry_url_1))
+    # @word_1 = doc_1.css('.hw').text.match(/^[a-zA-Z]+/)
+    # @definition_1 = doc_1.css('.ind').first.text
+    # @origin_1 = doc_1.css('.senseInnerWrapper p').text
+
+    # entry_url_2 = doc.css('td a')[@word_nos[1]].attribute('href').value.split(':').to_a.insert(1, 's:').join
+    # doc_2 = Nokogiri::HTML(open(entry_url_2))
+    # word_2 = doc_2.css('.hw').text.match(/^[a-zA-Z]+/)
+    # @definition_2 = doc_2.css('.ind').first.text
+
+    # entry_url_3 = doc.css('td a')[@word_nos[2]].attribute('href').value.split(':').to_a.insert(1, 's:').join
+    # doc_3 = Nokogiri::HTML(open(entry_url_3))
+    # word_3 = doc_3.css('.hw').text.match(/^[a-zA-Z]+/)
+    # @definition_3 = doc_3.css('.ind').first.text
+
+    # entry_url_4 = doc.css('td a')[@word_nos[3]].attribute('href').value.split(':').to_a.insert(1, 's:').join
+    # doc_4 = Nokogiri::HTML(open(entry_url_4))
+    # word_4 = doc_4.css('.hw').text.match(/^[a-zA-Z]+/)
+    # @definition_4 = doc_4.css('.ind').first.text
+    entry_urls = []
 
     entry_url_1 = doc.css('td a')[@word_nos[0]].attribute('href').value.split(':').to_a.insert(1, 's:').join
-    doc_1 = Nokogiri::HTML(open(entry_url_1))
-    @word_1 = doc_1.css('.hw').text.match(/^[a-zA-Z]+/)
-    @definition_1 = doc_1.css('.ind').first.text
-    @origin_1 = doc_1.css('.senseInnerWrapper p').text
-
-    entry_url_2 = doc.css('td a')[@word_nos[1]].attribute('href').value.split(':').to_a.insert(1, 's:').join
-    doc_2 = Nokogiri::HTML(open(entry_url_2))
-    word_2 = doc_2.css('.hw').text.match(/^[a-zA-Z]+/)
-    @definition_2 = doc_2.css('.ind').first.text
-
-    entry_url_3 = doc.css('td a')[@word_nos[2]].attribute('href').value.split(':').to_a.insert(1, 's:').join
-    doc_3 = Nokogiri::HTML(open(entry_url_3))
-    word_3 = doc_3.css('.hw').text.match(/^[a-zA-Z]+/)
-    @definition_3 = doc_3.css('.ind').first.text
-
-    entry_url_4 = doc.css('td a')[@word_nos[3]].attribute('href').value.split(':').to_a.insert(1, 's:').join
-    doc_4 = Nokogiri::HTML(open(entry_url_4))
-    word_4 = doc_4.css('.hw').text.match(/^[a-zA-Z]+/)
-    @definition_4 = doc_4.css('.ind').first.text
-
-# Conditional to check for empty strings.
-
-  [@word_1, @word_2, @word_3, @word_4, @definition_1, @definition_2, @definition_3, @definition_4, @origin_1].each do |word| 
-    if word == "" 
-      initialize
-    end
   end
 
-  # Set instance variables for Question objects.
+  # Check for empty strings.
+  def validate
+    [@word_1, @word_2, @word_3, @word_4, @definition_1, @definition_2, @definition_3, @definition_4, @origin_1].each do |word| 
+      if word == "" 
+        initialize
+      end
+  end
 
-  @definition = @definition_1
-  @definitions = [@definition_1, @definition_2, @definition_3, @definition_4]
-  @definitions_s = @definitions.shuffle 
+  # Return array of unique, pseudorandom index numbers to chose words from list.
+  def randomize
+    until @word_nos.length == 4
+      rand_no = rand(0..@length - 1)
+      @word_nos.include?(rand_no) ? randomize : @word_nos << rand_no
+    end
   end
 end
