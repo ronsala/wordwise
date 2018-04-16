fbf9b28f1af4793eed1a1c1d99d20fc1fdced605
RonSala <ron.sala@gmail.com>
2018-04-15 20:03:50 -0400

    Lint Question

--- lib/question.rb
+++ lib/question.rb
@@ -5,8 +5,6 @@ class Question
   attr_accessor :word_1, :definition_1, :definition_2, :definition_3, :definition_4, :definitions, :definitions_s, :length, :origin_1, :word_nos
 
   def initialize
-
-      # begin
       
         @word_nos = []
         doc = Nokogiri::HTML(open("https://en.oxforddictionaries.com/explore/weird-and-wonderful-words"))
@@ -19,7 +17,6 @@ class Question
 
         entry_url_1 = doc.css('td a')[@word_nos[0]].attribute('href').value.split(':').to_a.insert(1, 's:').join
         doc_1 = Nokogiri::HTML(open(entry_url_1))
-        # doc_1 = Nokogiri::HTML(open("https://en.oxforddictionaries.com/definition/suedehead")) #Temporary: Test for absent word origin.
         @word_1 = doc_1.css('.hw').text.match(/^[a-zA-Z]+/)
         @definition_1 = doc_1.css('.ind').first.text
         @origin_1 = doc_1.css('.senseInnerWrapper p').text
@@ -39,10 +36,6 @@ class Question
         word_4 = doc_4.css('.hw').text.match(/^[a-zA-Z]+/)
         @definition_4 = doc_4.css('.ind').first.text
 
-      # rescue StandardError => e
-      #   initialize
-      # end
-
     # Conditional to check for empty strings.
 
     [@word_1, @word_2, @word_3, @word_4, @definition_1, @definition_2, @definition_3, @definition_4, @origin_1].each do |word| 
@@ -56,8 +49,9 @@ class Question
     @definition = @definition_1
     @definitions = [@definition_1, @definition_2, @definition_3, @definition_4]
     @definitions_s = @definitions.shuffle 
-
-  # Return array of unique, pseudorandom index numbers to chose words from list on webpage.
+  end
+  # Return array of unique, pseudorandom index numbers to chose words from list 
+  # on webpage.
 
   def randomize
     until @word_nos.length == 4
@@ -65,5 +59,4 @@ class Question
       @word_nos.include?(rand_no) ? randomize : @word_nos << rand_no
     end
   end
-
 end
