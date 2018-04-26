259b62e0afcee343313883e13b42d4838f781534
RonSala <ron.sala@gmail.com>
2018-04-26 01:41:21 -0400

    Update wordwise.rb

--- lib/wordwise.rb
+++ lib/wordwise.rb
@@ -1,7 +1,6 @@
-# Is this file necessary?
 require 'wordwise/version'
 require './config/environment'
 
 # Create module for communication among classes.
-# module Wordwise
-# end
+module Wordwise
+end
