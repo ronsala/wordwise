a5cf22c1ad4d7814cdea1a2a9824b8281256fc93
RonSala <ron.sala@gmail.com>
2018-04-29 03:31:31 -0400

    Refactor Scraper

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -24,33 +24,33 @@ class Wordwise::Scraper
   def self.scrape_word_list(page_idx)
     @doc = Nokogiri::HTML(open(@list_urls[page_idx]))
     @words_ary = []
-    (0..@doc.css('td strong').length - 1).each do |i|
-      @words_ary << @doc.css('td strong')[i].text
+    (0..@doc.css('td').length - 1).each do |i|
+      @words_ary << @doc.css('td')[i].text
     end
   end
 
   # Sample 4 urls to words' pages and parse the question word, its origin and
   # definition, and 3 more definitions.
-  # def self.scrape_entry_pages
-  #   docs = []
-  #   @words = []
-  #   @defs = []
-
-  #   begin
-  #     question_urls = @entry_urls.sample(4)
-
-  #     question_urls.each_index do |i|
-  #       docs << Nokogiri::HTML(open(question_urls[i]))
-  #     end
-
-  #     docs.each_index do |i|
-  #       @words << docs[i].css('.hw').text.match(/^[a-zA-Z]+/).to_s
-  #       @defs << docs[i].css('.ind').first.text
-  #     end
-  #     @origin = docs[0].css('.senseInnerWrapper p').text
-  #     @question_array = [@words, @defs, @origin]
-  #   rescue NoMethodError => e
-  #     scrape_entry_pages
-  #   end
-  # end
+  def self.scrape_entry_pages
+    docs = []
+    @words = []
+    @defs = []
+    word_urls = []
+    begin
+      question_words = @words_ary[1..@words_ary.size].sample(4)
+      question_words.each_index do |i|
+        word_urls << "#{BASEPATH}/definition/#{question_words[i]}"
+        # docs << Nokogiri::HTML(open(question_urls[i]))
+      end
+binding.pry
+      docs.each_index do |i|
+        @words << docs[i].css('.hw').text.match(/^[a-zA-Z]+/).to_s
+        @defs << docs[i].css('.ind').first.text
+      end
+      @origin = docs[0].css('.senseInnerWrapper p').text
+      @question_array = [@words, @defs, @origin]
+    rescue NoMethodError => e
+      scrape_entry_pages
+    end
+  end
 end
