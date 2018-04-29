e21ea2235ed16f77956548880a74f3fa14268ba1
RonSala <ron.sala@gmail.com>
2018-04-29 10:39:32 -0400

    Add 'Switch list' functionality to CLI

--- lib/cli.rb
+++ lib/cli.rb
@@ -6,7 +6,6 @@ class Wordwise::CLI
   def call
     Wordwise::Scraper.scrape_word_lists
     introduction
-    play
   end
 
   # Display welcome message.
@@ -16,22 +15,27 @@ class Wordwise::CLI
     puts "You can learn more about the words in this quiz at https://www.oxforddictionaries.com.\n\n".center(80)
     puts "Get ready to test your word wisdom....\n\n".center(80)
     puts "What word list would you like to test your knowledge on?\n\n"
+    display_lists
+  end
+
+  def display_lists
+    puts ''
     Wordwise::Scraper.scrape_word_lists.each_with_index do |l, i|
       puts "     #{i + 1}) #{l}"
     end
-    puts "\nPlease enter the number of the list you want:\n"
     ask_list
   end
 
   def ask_list
+    puts "\nPlease enter the number of the list you want:\n"
     input = gets.strip.to_i
     size = Wordwise::Scraper.scrape_word_lists.size
     if input.between?(1, size)
       Wordwise::Scraper.scrape_word_list(input - 1)
     else
-      puts "Please enter a number 1 - #{size}:\n\n"
       ask_list
     end
+    play
   end
 
   # Display question and ask for answer.
@@ -82,7 +86,7 @@ class Wordwise::CLI
 
   # Display menu after user has answered a question.
   def ask_letter
-    puts "\nWord origin: 'o'. Next question: 'n'. Exit game: 'e'\n"
+    puts "\nWord origin: 'o'. Next question: 'n'. Switch list: 's'. Exit: 'e'\n"
     input = gets.strip.downcase
     case input
     when 'o'
@@ -90,6 +94,8 @@ class Wordwise::CLI
       ask_n_or_e
     when 'n'
       play
+    when 's'
+      display_lists
     when 'e'
       goodbye
     else
@@ -98,16 +104,18 @@ class Wordwise::CLI
   end
 
   # Display menu after origin has been displayed.
-  def ask_n_or_e
-    puts "Next question: 'n'. Exit game: 'e'\n"
+  def ask_n_s_or_e
+    puts "Next question: 'n'. Switch list: 's'. Exit game: 'e'\n"
     input = gets.strip.downcase
     case input
     when 'n'
       play
+    when 's'
+      display_lists
     when 'e'
       goodbye
     else
-      ask_n_or_e
+      ask_n_s_or_e
     end
   end
 
